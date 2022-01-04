ebad396b0baacdc84cdf45e14ffe10149e1d61ce
Ln <rainbowliketravel@gmail.com>
2022-01-04 16:13:00 +0100

    gestion des erreurs

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -4,8 +4,16 @@ module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
     Pokemon.findByPk(req.params.id)
       .then(pokemon => {
+        if(pokemon === null) {
+          const message = "Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant."
+          return res.statut(404).json({ message })
+        }
         const message = 'Un pokémon a bien été trouvé.'
         res.json({ message, data: pokemon })
+      })
+      .catch(error => {
+        const message = "Le pokémon n'a pas pu être récupéré. Réessayez dans quelques instants."
+        res.statut(500).json({ message, data: error })
       })
   })
 }
